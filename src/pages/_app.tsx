4da05da28a53607238e1ddc2faf5bd4df79c39ca
Edson Mallet <edsonmmr@gmail.com>
2021-02-11 18:48:44 -0300

    feat: add folders

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
+import { AppProps } from 'next/dist/next-server/lib/router/router'
 import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
-import GlobalStyle from '../styles/global'
-import { theme } from '../styles/theme'
+import GlobalStyle from '@styles/global'
+import { theme } from '@styles/theme'
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import Layout from '@components/Layout'
 
 const Home: React.FC = () => {
   return (
     <div>
-      <h1>Fluxo New</h1>
-
-      <Layout />
+      <h1>Fluxo</h1>
     </div>
   )
 }
