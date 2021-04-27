d6433c6f9ba71b72ea1bd397e424333f110001a4
Edson Mallet <edsonmmr@gmail.com>
2021-04-26 23:43:56 -0300

    feat: answer in dev

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,6 @@ import axios from 'axios'
 import Cookies from 'js-cookie'
 
 const BASE_URL = 'https://dev-api-fluxo-v2.herokuapp.com/api/v1'
-//const BASE_URL = 'http://127.0.0.1:3333/api/v1'
 
 const api = axios.create({
   baseURL: BASE_URL
